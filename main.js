a09a75b2b6efccce7e4e0fa600c5c3ed2efd0c5f
imyanyijie <yanyijie1993@gmail.com>
2024-02-12 10:43:29 -0500

    added the popup form to add new book
    added wireframe for popup
    
    added js function to add books to library

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const container = document.querySelector(".main-container");
-const addBook = document.querySelector(".add");
-
+const container = document.querySelector(".main-containter");
+const addButton = document.querySelector(".add");
+const popup = document.querySelector(".popup-form");
+const titleChoice = document.querySelector("#title");
+const authorChoice = document.querySelector("#author");
+const checkRead = document.querySelector(".checkbox")
 const library = []
 
 let title;
@@ -8,18 +11,28 @@ let author;
 let page;
 let readStatus;
 
-function addBook(){
-    title = titleChoice.value;
-    author = authorChoice.value;
-    genre = genreChoice.value;
-    page = pageChoice.value;
-    bookName = new Book(title, author, genre, page)
-    myLibrary.push(bookName);
-    console.log(myLibrary)
+// hidePopup();
+showPopup();
+
+addBook("Author1", "Title1", "1", true);
+addBook("Author2", "Title2", "2", false);
+addBook("Author3", "Title3", "3", true);
+addBook("Author4", "Title4", "4", false);
+
+showBook();
+showBook();
+showBook();
+showBook();
+
+
+
+function addBook(title, author, page, readStatus){
+    bookName = new Book(title, author, page, readStatus)
+    library.push(bookName);
+    console.log(library)
     console.log(bookName.author)
     console.log(bookName)
 }
-
 
 function Book(title, author, page, readStatus){
     this.title =title;
@@ -27,27 +40,34 @@ function Book(title, author, page, readStatus){
     this.page = page;
     this.readStatus = readStatus;
 }
-
 function showBook(){
     const div =document.createElement('div');
     div.classList.add("card");
     const h1 = document.createElement('p')
-    h1.textContent = title;
+    h1.classList.add("title");
+    // h1.textContent = title;
+    h1.textContent = "test title"
+
     const div1 = document.createElement('p')
     div1.innerHTML = `<span>author:</span> ${author}`
     const div3 = document.createElement('p')
-    div3.innerHTML = `<span>Current page:</span><input class="pageinput" value="${page}"/>`
+    div3.innerHTML = `<span>Current page:</span>${page}`
     const completeButton = document.createElement('button')
     completeButton.classList.add("completeButton")
     completeButton.textContent = "Mark Complete"
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add("delete");
+    deleteButton.textContent = "Delete"
 
-    div.appendChild(h1)
-    div.appendChild(div1)
-    div.appendChild(div3)
-    div.appendChild(completeButton)
-    
-    container.appendChild(div)
-  
+    div.appendChild(h1);
+    div.appendChild(div1);
+    div.appendChild(div3);
+    div.appendChild(completeButton);
+    div.appendChild(deleteButton);
+    container.appendChild(div);
+
+
+
     deleteButton.addEventListener("click", () =>{bookstore.removeChild(div)});
   
     completeButton.addEventListener("click", () =>{
@@ -56,6 +76,14 @@ function showBook(){
       div.insertBefore(completed, completeButton)
       div.removeChild(completeButton)
     })
+
+  }
+
+  function hidePopup(){
+    popup.style.display = "none";
+  }
   
+  function showPopup(){
+    popup.style.display = "block";
   }
   
